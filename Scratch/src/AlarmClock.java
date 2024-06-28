e0a09b4dab67aea69fbe70300b23ce9bc71cef80
Cortney B <cortneyb@tlgcohort.com>
2024-06-28 17:13:53 -0400

    added private fields, getter/setter and encapsulation

--- Scratch/src/AlarmClock.java
+++ Scratch/src/AlarmClock.java
@@ -5,10 +5,31 @@
  */
 class AlarmClock {
     // properties or attributes - these are called "instance variables" or "fields" in Java
-    int snoozeInterval = 5; //default value when client doesn't specify one (instead of default of 0)
+    // accessor methods - provide controlled access to the objects fields
+    private int snoozeInterval = 5; //default value when client doesn't specify one (instead of default of 0)
 
     // functions or operations - these are called "methods" in Java
-    void snooze() {
+    public void snooze() {
         System.out.println("Snoozing for " + snoozeInterval + " minutes");
     }
+
+    // accessor methods - provide "controlled access" to the objects fields
+    public int getSnoozeInterval() {
+        return snoozeInterval;
+    }
+    // TODO: implement constraint - must be between 1 and 20 inclusive
+    // if the incoming value is "valid" we take it, assign to the private field
+    // otherwise we reject w/ an error message
+    public void setSnoozeInterval(int snoozeInterval) {
+        this.snoozeInterval = snoozeInterval;
+    }
+//    public void setSnoozeInterval(int snoozeIntervalIn) {
+//    snoozeInterval = snoozeIntervalIn;
+//    }
+
+    // Purpose: return a string (sentence) describing this object
+    public String toString() {
+        return "AlarmClock: snoozeInterval = " + snoozeInterval;
+    }
+
 }
--- Scratch/src/AlarmClockClient.java
+++ Scratch/src/AlarmClockClient.java
@@ -10,20 +10,26 @@ class AlarmClockClient {
     public static void main(String[] args) {
         //create an instance (or object) of AlarmClock and set its snoozeInterval
         AlarmClock clock1 = new AlarmClock();
-        clock1.snoozeInterval = 7;
+        clock1.setSnoozeInterval(7);
 
         //second AlarmClock object and set its snoozeInterval also
         AlarmClock clock2 = new AlarmClock();
-        clock2.snoozeInterval = 10;
+        clock2.setSnoozeInterval(10);
 
         // create third AlarmClock object, don't give it a snooze interval
         // defaults to 0
         AlarmClock clock3 = new AlarmClock();
 
 
-        //make them snooze()
+        // make them snooze()
         clock1.snooze();
         clock2.snooze();
         clock3.snooze();
+
+        System.out.println(clock1.toString());
+        System.out.println(clock2); // toString() automatically is called
+        System.out.println(clock3);
+        // show their toString() methods in action
+
     }
 }
