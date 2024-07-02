4eff6c76ad82b8259dc8070381be013f8e082bbc
Cortney B <cortneyb@tlgcohort.com>
2024-07-02 12:25:10 -0400

    Added public static constants for min and max levels

--- WhiteboardModel/src/PokemonValidationTest.java
+++ WhiteboardModel/src/PokemonValidationTest.java
@@ -1,12 +1,14 @@
 // Validates Pokemon is assigned an appropriate level in setLvl() method
 class PokemonValidationTest {
     public static void main(String[] args) {
+
+        // Pokemon level testing
         Pokemon pkmn = new Pokemon();
 
-        pkmn.setLvl(1); // value should "stick" and be 1, no error message
+        pkmn.setLvl(Pokemon.MIN_LVL); // value should "stick" and be 1, no error message
         System.out.println(pkmn);
 
-        pkmn.setLvl(100); // value should "stick" and be 100, no error message
+        pkmn.setLvl(Pokemon.MAX_LVL); // value should "stick" and be 100, no error message
         System.out.println(pkmn);
 
         pkmn.setLvl(0); // error message, value rejected (value is still 100)
@@ -19,11 +21,12 @@ class PokemonValidationTest {
 
         // pokemon evolution testing
         Pokemon squirtle = new Pokemon("Squirtle");
-        System.out.println("Pokemon (Squirtle): " + squirtle.getSpecies());
+        System.out.println("Pokemon: " + squirtle.getSpecies());
         squirtle.evolve();
 
-        Pokemon random = new Pokemon("Random");
-        System.out.println("Pokemon (Random): " + random.getSpecies());
-        random.evolve();
+        Pokemon invalidSpecies = new Pokemon("Random");
+        System.out.println("Pokemon: " + invalidSpecies.getSpecies());
+        invalidSpecies.evolve();
     }
 }
+
