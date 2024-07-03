891ee6bb855d2cd365209d9ac8bb6ff56c416766
Cortney B <cortneyb@tlgcohort.com>
2024-07-03 13:16:41 -0400

    using wrapper classes to convert from string into proper types: int, Integer

---
+++ Scratch/src/WrappersTest.java
@@ -0,0 +1,24 @@
+class WrappersTest {
+    public static void main(String[] args) {
+        String ageInput = "28";
+        int age = Integer.parseInt(ageInput); // returns int 28 from string "57"
+        Integer ageInteger = Integer.valueOf(ageInput); // returns object Integer(28) from "28"
+
+        String populationInput = "8000000000";
+        long population = Long.parseLong(populationInput);
+        Long populationLong = Long.valueOf(populationInput);
+
+        String priceInput = "12.49";
+        double price = Double.parseDouble(priceInput);
+        Double priceDouble = Double.valueOf(priceInput);
+
+        String isSunnyInput = "true";
+        boolean isSunny = Boolean.parseBoolean(isSunnyInput);
+        Boolean sunnyBoolean = Boolean.valueOf(isSunnyInput);
+
+        System.out.println(ageInteger);
+        System.out.println(populationLong);
+        System.out.println(priceDouble);
+        System.out.println(sunnyBoolean);
+    }
+}
