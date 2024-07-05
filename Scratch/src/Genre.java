80a6b5775e73c23619e50ead4ba43df127deed99
Cortney B <cortneyb@tlgcohort.com>
2024-07-05 13:05:40 -0400

    smart enum

--- Scratch/src/Genre.java
+++ Scratch/src/Genre.java
@@ -1,3 +1,26 @@
 enum Genre {
-    ANIME, HORROR, DRAMA, COMEDY, ACTION, SCI_FI, CARTOON
+    // each of these is calling the Genre() ctor below
+    ANIME("Anime"),
+    HORROR("Horror"),
+    DRAMA("Drama"),
+    COMEDY("Comedy"),
+    ACTION("Action"),
+    SCI_FI("Sci-Fi"),
+    CARTOON("Cartoon");
+
+    // everything under here is just regular class definition stuff, fields, ctos and methods..
+    private String display;
+
+    // must be implicitly private
+   Genre(String display) {
+       System.out.println("Genre ctor called.");
+       this.display = display;
+    }
+    public String getDisplay() {
+       return display;
+    }
+
+    public String toString() {
+       return getDisplay();
+    }
 }
--- Scratch/src/Rating.java
+++ Scratch/src/Rating.java
@@ -1,3 +1,22 @@
 enum Rating {
-    G, PG, PG_13, R, NC_17
+    G("G"),
+    PG("PG"),
+    PG_13("PG-13"),
+    R("R"),
+    NC_17("NC-17");
+
+    private String output;
+
+    // must be implicitly private
+    Rating(String output) {
+        System.out.println("Rating ctor called.");
+        this.output = output;
+    }
+    public String getOutput() {
+        return output;
+    }
+
+    public String toString() {
+        return getOutput();
+    }
 }
