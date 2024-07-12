967214e2b6859dddea38c3ccace44eecba2f2936
Cortney B <cortneyb@tlgcohort.com>
2024-07-12 16:53:07 -0400

    Fixed error in BoatClient

--- Lab10-Extra/src/com/transportation/BoatClient.java
+++ Lab10-Extra/src/com/transportation/BoatClient.java
@@ -2,7 +2,7 @@ package com.transportation;
 
 class BoatClient {
     public static void main(String[] args) {
-        Vehicle b = new Boat("Beansie Boat");
+        Boat b = new Boat("Beansie Boat");
         b.moveTo("West Seattle");
     }
 }
