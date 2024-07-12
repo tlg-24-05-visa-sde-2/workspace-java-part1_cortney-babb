a3bddd841063dfbec2fd354e43ef23dbc9e2ff94
Cortney B <cortneyb@tlgcohort.com>
2024-07-12 11:51:06 -0400

    Throws exception in HourlyEmployee

--- Lab10.1/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1/src/com/hr/personnel/HourlyEmployee.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
     // fields
+    private static final double FEDERAL_MINIMUM_WAGE = 15.0;
     private double rate;
     private double hours;
 
@@ -23,7 +24,8 @@ public class HourlyEmployee extends Employee {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours)
+    throws IllegalArgumentException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -45,8 +47,13 @@ public class HourlyEmployee extends Employee {
     public double getRate() {
         return rate;
     }
-
-    public void setRate(double rate) {
+    // TODO implement setter validation
+    public void setRate(double rate) throws IllegalArgumentException {
+        if (rate < FEDERAL_MINIMUM_WAGE) {
+            throw new IllegalArgumentException("Error: Illegal wage: " + rate +
+                    ". Federal minimum rate must be at least " +
+                    FEDERAL_MINIMUM_WAGE + " or greater.");
+        }
         this.rate = rate;
     }
 
